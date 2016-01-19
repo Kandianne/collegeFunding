7908c541cd60fa90f5a7b184945a96c2b393f866
kandianne <kandipierre@gmail.com>
2016-01-19 14:55:45 -0800

    laid out scholarship rewards and ngRepeat working properly

--- public/javascript/controllers/SuccessController.js
+++ public/javascript/controllers/SuccessController.js
@@ -1,6 +1,6 @@
 
 (function() {
-	'use strict';
+	// 'use strict';
 	angular.module('app')
 	.controller('SuccessController', SuccessController);
 
@@ -8,15 +8,16 @@
 		var vm = this;
 
 		vm.returnScholarships = function(){
-			vm.success = [{
-				colleges: ['Allegheny College', 'Assumption College', 'Azusa Pacific University'],
-				offers: ['39,565', '21,500', '22,558'],
-				projections: ['158,260', ' 86,000', ' 90,232'] 
-			}];
-			console.log(vm.success);
+			vm.colleges = ['Allegheny College', 'Assumption College', 'Azusa Pacific University'];
+			vm.offers = ['39,565', '21,500', '22,558'];;
+			vm.projections = ['158,260', ' 86,000', ' 90,232'] 
+		
+			console.log(vm.colleges);
+			console.log(vm.offers);
+			console.log(vm.projections);
 		};
 
 		vm.returnScholarships();
-
+		
 	}
 })();
