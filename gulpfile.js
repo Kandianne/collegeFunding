2d0d5df07df9816d9abd4460b6acd6d854bb9dcb
Kandianne <kandipierre@gmail.com>
2016-02-04 17:11:26 -0800

    minified pics

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
+var imagemin = require('gulp-imagemin');
 
 //==================TASKS THAT GULP CAN PERFORM=====================================
+
+//IMAGE MIN
+gulp.task('jpgs', function() {
+    return gulp.src('public/images/*.jpg')
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest('public/minImages'));
+});
+
 
 //BROWSER SYNC
 gulp.task('browserSync', function() {
